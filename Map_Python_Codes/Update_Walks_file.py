17b87bd9cc21619a0c30f92025172d7d586209e8
Rory Yarr <roryyarr314@gmail.com>
2024-07-16 21:53:27 +1000

    Update Update_Walks_file.py

--- Map_Python_Codes/Update_Walks_file.py
+++ Map_Python_Codes/Update_Walks_file.py
@@ -4,19 +4,20 @@ import pandas as pd
 # Google Sheets URL
 
 
-sheet_url = 'https://docs.google.com/spreadsheets/d/1mGR_xugxcg3Pc3e1KLzggZn6XfnSJOuHncZ64hOo8M4/export?format=csv'
+sheet_url = "https://docs.google.com/spreadsheets/d/1mGR_xugxcg3Pc3e1KLzggZn6XfnSJOuHncZ64hOo8M4/export?format=csv&gid=0"
 
 # Read the data into a DataFrame
 df = pd.read_csv(sheet_url)
 df
 
-# Extract data from the DataFrame
+
 names = df['Name'].tolist()
 locations = df[['Latitude', 'Longitude']].values.tolist()
 links = df['Google_Link'].tolist()
+routes = df['Route_Link'].tolist()
 addresses = df['Address'].tolist()
 descriptions = df['Description'].tolist()
-routes = df['Route_Links'].tolist()
+lengths = df['Length']
 icons = df['Icon'].tolist()
 colours = df['Colour'].tolist()
 
@@ -24,17 +25,23 @@ colours = df['Colour'].tolist()
 m = folium.Map(location=(-32.9, 151.79), zoom_start=11)
 
 # Add markers for each location
-for name, loc, link, address, desc, icon, colour, route in zip(names, locations, links, addresses, descriptions, icons, colours, routes):
+for name, loc, link, route, address, desc, len, icon, colour in zip(names, locations, links, routes, addresses, descriptions, lengths, icons, colours):
     # Set a default icon if the icon field is blank
     if pd.isna(icon) or icon == "":
         icon = 'info-sign'  # Default icon
 
+    # Replaces NaN with empty strings
+    desc = "" if pd.isna(desc) else desc
+    #route = "" if pd.isna(route) else route
+    len = "" if pd.isna(len) else len
+
     custom_icon = folium.Icon(color = colour, icon=icon, prefix='fa')
 
+    route_link = f"<a href='{route}' target='_blank'>Route Map Link</a><br>" if not pd.isna(route) else ""
 
     folium.Marker(
         location=loc,
-        popup=f"<b>{name}</b><br><a href='{link}' target='_blank'>Google Maps Link</a><br>{address}<br>{desc}<br><a href='{route}' target='_blank'>Route Map Link</a>",
+        popup = f"<b>{name}</b><br>{route_link}{desc}<br>",
         tooltip=name,
         icon = custom_icon
     ).add_to(m)
