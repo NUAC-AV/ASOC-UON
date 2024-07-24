2d9e05fb8f20f773f005fa6af66719d6b7dfd638
Rory Yarr <roryyarr314@gmail.com>
2024-07-24 10:24:02 +1000

    Update letterbox map.

--- Map_Generating_Codes/Letterbox/main.py
+++ Map_Generating_Codes/Letterbox/main.py
@@ -31,13 +31,3 @@ map_control.add_tree_layer_control()
 output_html = "Maps/Letterbox/letterbox_test_1.html"
 map_control.save_map(output_html)
 
-# Display the map in a Jupyter notebook (optional)
-#map_with_tree.display_map()
-
-
-
-#map_control = MapWithTreeLayerControl(base_places)
-#map_control.add_gpx_route(gpx_file, layer_name="My GPX Route")
-#map_control.add_tree_layer_control()
-#map_control.save_map(output_html)
-#map_control.display_map()
